22594ee4ca5fc65a6c5aa67fa57fd6c49dd300e3
Bryan Gadd <x-dragola-x@live.ca>
2023-02-20 12:59:13 -0500

    Started working on Q3- Figuring out the argparse
    Issues:
    - args.lambda is being detected as something else. Will look into that.
    - Need to figure out how to parse an array as an argument (nodes).

---
+++ A2/Assignment 2 Question 3/src/page_rank.py
@@ -0,0 +1,36 @@
+import argparse
+
+def page_rank(maxiteration, lamb, thr/*, nodes*/):
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Command Line Interface for Wikipedia Processing"
+    )
+    parser.add_argument(
+        "--maxiteration",
+        help="Maximum number of iterations",
+        type=int,
+        required=True,
+    )
+    parser.add_argument(
+        "--lambda",
+        help="The lambda",
+        type=float,
+        required=True,
+    )
+    parser.add_argument(
+        "--thr",
+        help="Threshold",
+        type=float,
+        required=True,
+    )
+    # parser.add_argument(
+    #     "--nodes",
+    #     help="NodeIDs that are retrieved at the end with their page ranks",
+    #     type=arr,
+    #     default=False,
+    #     required=False,
+    # )
+    args = parser.parse_args()
+    page_rank(args.maxiteration, args.lambda, args.thr/*, args.nodes*/)
