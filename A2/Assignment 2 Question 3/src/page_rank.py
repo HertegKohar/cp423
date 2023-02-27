0683f2e72007f4e8a21d99b450be15b012fadc4b
Bryan Gadd <x-dragola-x@live.ca>
2023-02-26 23:04:31 -0500

    Hopefully got lambda working, not sure about nodes...

--- A2/Assignment 2 Question 3/src/page_rank.py
+++ A2/Assignment 2 Question 3/src/page_rank.py
@@ -1,9 +1,10 @@
 import argparse
 
-def page_rank(maxiteration, lamb, thr/*, nodes*/):
-#ignore any lines that start with # (few before you reach the actual nodes).
+def page_rank(maxiteration, lamb, thr, nodes):
+    #ignore any lines that start with # (few before you reach the actual nodes).
 
-#need to perform pagerank on nodes...
+    #need to perform pagerank on nodes...
+    open("Web-Stanford.txt", "r")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -20,6 +21,8 @@ if __name__ == "__main__":
         help="The lambda",
         type=float,
         required=True,
+        dest='lambda_',
+        #metavar='LAMBDA',
     )
     parser.add_argument(
         "--thr",
@@ -27,12 +30,14 @@ if __name__ == "__main__":
         type=float,
         required=True,
     )
-    # parser.add_argument(
-    #     "--nodes",
-    #     help="NodeIDs that are retrieved at the end with their page ranks",
-    #     type=arr,
-    #     default=False,
-    #     required=False,
-    # )
+    parser.add_argument(
+        "--nodes",
+        help="NodeIDs that are retrieved at the end with their page ranks",
+        nargs='+',
+        required=False,
+    )
     args = parser.parse_args()
-    page_rank(args.maxiteration, args.lambda, args.thr/*, args.nodes*/)
+    #page_rank(args.maxiteration, args.lambda_, args.thr, args.nodes)
+    
+    #test values
+    page_rank(2, 0.15, args.thr, args.nodes)
