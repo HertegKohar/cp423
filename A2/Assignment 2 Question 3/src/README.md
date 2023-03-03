809f3e1f87a29b18cb2d7b776b33abad40847596
Bryan Gadd <x-dragola-x@live.ca>
2023-03-03 15:35:19 -0500

    Reading the file and gettin the numbers
    Changes:
    - Fixed README.md (spaces and wrong argument names).
    - Added some notes/thoughts about how the function needs to be implemented.
    - Added ability to read the nodes from the file (prints the node and it's linked node for testing.

--- A2/Assignment 2 Question 3/src/README.md
+++ A2/Assignment 2 Question 3/src/README.md
@@ -1,6 +1,6 @@
 **Program Instructions**  
 Sample Command Line Use (all fields are required)
 ```Bash
-python page_rank.py --maxiterations 5 -- lambda 0.15 --thr 0.1 --nodes [5,100,50]
+python page_rank.py --maxiteration 5 --lambda 0.15 --thr 0.1 --nodes [5,100,50]
 ```
 
--- A2/Assignment 2 Question 3/src/page_rank.py
+++ A2/Assignment 2 Question 3/src/page_rank.py
@@ -5,13 +5,35 @@ import ast
 Author: Bryan Gadd
 """
 
+"""
+Notes/thoughts:
+ - Only loop through the txt file once
+ - Store and update the nodes (store id, PR, nodes it points to, etc.)
+ - For each iteration is just re-loops through the existing stored nodes
+"""
 def page_rank(maxiteration, lambda_, thr, nodes):
     #test output to make sure variables are good
     print("maxiteration= " + str(maxiteration) + ", lambda_= " + str(lambda_) + ", thr= " + str(thr) + ", nodes= " + str(nodes))
     
     file = open("Web-Stanford.txt", "r")
+    line = file.readline()
 
-    #ignore any lines that start with # (few before you reach the actual nodes).
+    #get to actual nodes
+    while (line[0].isdigit() == False):
+        line = file.readline()
+
+    #until end of file
+    while (line != ""):
+        #extract node number and the node it points to
+        extractedString = line.split()
+        nodeN = int(extractedString[0])
+        nodeConn = int(extractedString[1])
+
+        #verifying I can get the numbers
+        print(str(nodeN) + " --> "+ str(nodeConn))
+
+        #next line
+        line = file.readline()
 
     file.close()
 
