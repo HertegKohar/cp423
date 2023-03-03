9a0ebdfb95d6c3654fe648e4438ca2e99fd51ccc
Herteg Kohar <koha5160@mylaurier.ca>
2023-03-03 12:59:34 -0500

    Similar to before, extra features included

--- A2/Assignment 2 Question 1/src/wikipedia_processing.py
+++ A2/Assignment 2 Question 1/src/wikipedia_processing.py
@@ -1,24 +1,23 @@
 """
 Author: Herteg Kohar
 """
-from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize, RegexpTokenizer
+from nltk.corpus import stopwords, PlaintextCorpusReader
 from nltk.stem import PorterStemmer
 from collections import Counter, defaultdict
 import matplotlib.pyplot as plt
 import nltk
-import string
 import json
 import argparse
 import os
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import time
 
 
 def load_articles(path):
     """Load articles from the given path to directory for wikipedia data
-
     Args:
         path (str): Path to directory containing wikipedia data
-
     Returns:
         list[dict]: List of articles
         str: The corpus of all articles
@@ -36,6 +35,139 @@ def load_articles(path):
             all_articles.extend(articles)
         break
     return corpus, all_articles
+
+
+def load_articles_incremented(path, increment):
+    """Load articles from the given path to directory for wikipedia data
+
+    Args:
+        path (str): Path to directory containing wikipedia data
+
+    Returns:
+        list[dict]: List of articles
+        str: The corpus of all articles
+    """
+    file_paths = []
+    for i, file in enumerate(os.listdir(path)):
+        file_paths.append(f"{path}/{file}")
+        if i % increment == 0 and i != 0:
+            yield file_paths
+            file_paths = []
+    yield file_paths
+
+
+def write_article_text(article, path):
+    with open(f"{path}/{article['id']}.txt", "w", encoding="utf-8") as f:
+        f.write(article["text"])
+
+
+def load_article_texts_threaded(path):
+    start = time.time()
+    count = 0
+    if not os.path.exists("article_texts"):
+        os.mkdir("article_texts")
+    for file in os.listdir(path):
+        with open(f"{path}/{file}", encoding="utf-8") as input_file:
+            articles = json.load(input_file)
+            with ThreadPoolExecutor() as executor:
+                futures = [
+                    executor.submit(write_article_text, article, "article_texts")
+                    for article in articles
+                ]
+                for _ in as_completed(futures):
+                    pass
+        count += 1
+        print("Finished file: ", count)
+    end = time.time()
+    print(f"Time taken: {end - start}")
+
+
+def load_corpus(path):
+    fileids = os.listdir(path)
+    corpus = PlaintextCorpusReader(path, fileids, encoding="utf-8")
+    return corpus
+
+
+def tokenize_generator(tokenizer, text):
+    for token in tokenizer.tokenize(text):
+        yield token
+
+
+def tokenize_threaded(corpus):
+    start = time.time()
+    count = 0
+    chunk_size = 10000
+    max_workers = os.cpu_count()
+    tokenizer = RegexpTokenizer(r"\w+")
+    futures = []
+    with open("wikipedia.token", "a", encoding="utf-8") as f:
+        with ThreadPoolExecutor(max_workers=max_workers) as executor:
+            for i, fileid in enumerate(corpus.fileids()):
+                if (i + 1) % chunk_size == 0:
+                    # If memory error write to file here
+                    for future in as_completed(futures):
+                        for token in future.result():
+                            f.write(token)
+                            f.write("\n")
+                    count += 1
+                    print("Finished chunk: ", count)
+                    futures = []
+                futures.append(
+                    executor.submit(tokenize_generator, tokenizer, corpus.raw(fileid))
+                )
+            print("Last chunk")
+            if futures:
+                for future in as_completed(futures):
+                    for token in future.result():
+                        f.write(token)
+                        f.write("\n")
+                futures.clear()
+    end = time.time()
+    print(f"Time taken: {end - start}")
+
+
+def stemming_generator(porter_stemmer, tokenizer, text):
+    for token in tokenize_generator(tokenizer, text):
+        yield porter_stemmer.stem(token)
+
+
+def tokenize_and_stemm_threaded(corpus):
+    start = time.time()
+    count = 0
+    chunk_size = 10000
+    max_workers = os.cpu_count()
+    porter_stemmer = PorterStemmer()
+    futures = []
+    tokenizer = RegexpTokenizer(r"\w+")
+    with open("wikipedia.token.stemm", "a", encoding="utf-8") as f:
+        with ThreadPoolExecutor(max_workers=max_workers) as executor:
+            for i, fileid in enumerate(corpus.fileids()):
+                if (i + 1) % chunk_size == 0:
+                    # If memory error write to file here
+                    for future in as_completed(futures):
+                        for token in future.result():
+                            f.write(token)
+                            f.write("\n")
+                    count += 1
+                    print("Finished chunk: ", count)
+                    futures = []
+                futures.append(
+                    executor.submit(
+                        stemming_generator,
+                        porter_stemmer,
+                        tokenizer,
+                        corpus.raw(fileid),
+                    )
+                )
+            print("Last chunk")
+            if futures:
+                for future in as_completed(futures):
+                    for token in future.result():
+                        f.write(token)
+                        f.write("\n")
+                futures.clear()
+    end = time.time()
+    print(f"Time taken: {end - start}")
 
 
 def plot_zipfs(tokens):
@@ -58,9 +190,11 @@ def plot_zipfs(tokens):
     return
 
 
-def tokenize_corpus(tokens):
-    with open("wikipedia.token", "w", encoding="utf-8") as f:
-        f.write(" ".join(tokens))
+def save_tokenized_corpus(tokens):
+    with open("wikipedia.token", "a", encoding="utf-8") as f:
+        for token in tokens:
+            f.write(token)
+            f.write("\n")
     return
 
 
@@ -70,10 +204,11 @@ def remove_stop_words(tokens):
     Args:
         tokens (list[str]): List of tokens from the corpus
     """
-    stop_words = set(stopwords.words("english") + list(string.punctuation))
+    stop_words = set(stopwords.words("english"))
     filtered_tokens = [token for token in tokens if not token.lower() in stop_words]
-    with open("wikipedia.token.stop", "w", encoding="utf-8") as f:
+    with open("wikipedia.token.stop", "a", encoding="utf-8") as f:
         f.write(" ".join(filtered_tokens))
+        f.write("\n")
     return
 
 
@@ -85,8 +220,9 @@ def stemming(tokens):
     """
     porter_stemmer = PorterStemmer()
     stemmed_words = [porter_stemmer.stem(token) for token in tokens]
-    with open("wikipedia.token.stemm", "w", encoding="utf-8") as f:
+    with open("wikipedia.token.stemm", "a", encoding="utf-8") as f:
         f.write(" ".join(stemmed_words))
+        f.write("\n")
     return
 
 
@@ -108,6 +244,105 @@ def create_inverted_index(articles):
     with open("inverted_index.json", "w", encoding="utf-8") as f:
         json.dump(inverted_index, f)
     return inverted_index
+
+
+def open_and_create_corpus(path):
+    with open(path, "r", encoding="utf-8") as f:
+        articles = json.load(f)
+        corpus = " ".join([article["text"] for article in articles])
+        tokens = word_tokenize(corpus)
+    return tokens
+
+
+def open_and_create_corpus_threaded(path, max_workers):
+    with open(path, "r", encoding="utf-8") as f:
+        articles = json.load(f)
+        with ThreadPoolExecutor(max_workers=max_workers) as executor:
+            futures = [
+                executor.submit(word_tokenize, article["text"]) for article in articles
+            ]
+            for future in as_completed(futures):
+                yield future.result()
+
+
+def tokenize_and_stem(text, porter_stemmer):
+    for token in word_tokenize(text):
+        yield porter_stemmer.stem(token)
+
+
+def stemming_threaded(path, porter_stemmer, max_workers):
+    with open(path, "r", encoding="utf-8") as f:
+        articles = json.load(f)
+        with ThreadPoolExecutor(max_workers=max_workers) as executor:
+            futures = [
+                executor.submit(tokenize_and_stem, article["text"], porter_stemmer)
+                for article in articles
+            ]
+            for future in as_completed(futures):
+                yield future.result()
+
+
+def perform_tasks_threaded(**kwargs):
+    if kwargs["tokenize"]:
+        count = 0
+        max_workers = 3
+        start = time.time()
+        with open("wikipedia.token", "a", encoding="utf-8") as f:
+            for file_paths in load_articles_incremented("data_wikipedia", max_workers):
+                with ThreadPoolExecutor(max_workers=max_workers) as executor:
+                    futures = [
+                        executor.submit(
+                            open_and_create_corpus_threaded, path, max_workers + 1
+                        )
+                        for path in file_paths
+                    ]
+                    for future in as_completed(futures):
+                        count += 1
+                        for token in future.result():
+                            f.write(token)
+                            f.write("\n")
+                        print("Processed", count, "files")
+        print("Tokenized corpus in", time.time() - start, "seconds")
+    if kwargs["stemming"]:
+        # Don't open the file just tokenize and stem in the multi-level threading (second level)
+        # Opening token file makes it too slow
+        porter_stemmer = PorterStemmer()
+        max_workers = 3
+        count = 0
+        start = time.time()
+        with open("wikipedia.token.stemm", "a", encoding="utf-8") as f:
+            for file_paths in load_articles_incremented("data_wikipedia", max_workers):
+                with ThreadPoolExecutor(max_workers=max_workers) as executor:
+                    futures = [
+                        executor.submit(
+                            stemming_threaded, path, porter_stemmer, max_workers + 1
+                        )
+                        for path in file_paths
+                    ]
+                    for future in as_completed(futures):
+                        count += 1
+                        for token in future.result():
+                            f.write(" ".join(token))
+                            f.write("\n")
+                        print("Processed", count, "files")
+        end = time.time()
+        print("Time taken for stemming:", end - start)
+
+        # print("Processing", len(articles), "articles")
+        # for article in articles:
+        #     tokens = word_tokenize(article["text"])
+        #     if kwargs["stopwords"]:
+        #         remove_stop_words(tokens)
+        #     if kwargs["tokenize"]:
+        #         tokenize_corpus(tokens)
+        #     if kwargs["stemming"]:
+        #         stemming(tokens)
+        # count += len(articles)
+        # print("Processed", count, "articles")
+    # if kwargs["invertedindex"]:
+    #     create_inverted_index(all_articles)
+    # if kwargs["zipf"]:
+    #     plot_zipfs(tokens)
 
 
 if __name__ == "__main__":
@@ -150,23 +385,41 @@ if __name__ == "__main__":
         required=False,
     )
     args = parser.parse_args()
-    corpus, all_articles = load_articles("wikipedia_data")
-    tokens = word_tokenize(corpus)
+    # Clear token files
+    if args.tokenize:
+        with open("wikipedia.token", "w", encoding="utf-8") as f:
+            pass
+    if args.stopwords:
+        with open("wikipedia.token.stop", "w", encoding="utf-8") as f:
+            pass
+    if args.stemming:
+        with open("wikipedia.token.stemm", "w", encoding="utf-8") as f:
+            pass
+    # print("Loading corpus")
+    # start = time.time()
+    # corpus = load_corpus("article_texts")
+    # end = time.time()
+    # print("Time taken to load corpus:", end - start)
+    corpus, all_articles = load_articles("data_wikipedia")
+    tokens = RegexpTokenizer(r"\w+").tokenize(corpus)
     if args.stopwords:
         print("Removing stopwords")
         nltk.download("stopwords")
-        stop_words = set(stopwords.words("english"))
         remove_stop_words(tokens)
-    if args.zipf:
-        print("Plotting Zipf's Law")
-        plot_zipfs(tokens)
+        print("Saved corpus without stopwords to wikipedia.token.stop")
     if args.tokenize:
         print("Tokenizing corpus")
-        tokenize_corpus(tokens)
+        save_tokenized_corpus(tokens)
+        print("Saved tokenized corpus to wikipedia.token")
     if args.stemming:
         print("Applying stemming")
         nltk.download("punkt")
         stemming(tokens)
+        print("Saved stemmed corpus to wikipedia.token.stemm")
     if args.invertedindex:
         print("Creating inverted index")
         create_inverted_index(all_articles)
+        print("Saved inverted index to inverted_index.json")
+    if args.zipf:
+        print("Plotting Zipf's Law")
+        plot_zipfs(tokens)
