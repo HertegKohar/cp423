ac84103cafb6a543853c69f5ee23795309d2d858
Bryan Gadd <x-dragola-x@live.ca>
2023-03-03 17:22:58 -0500

    Attempting to store nodes in objects...

--- A2/Assignment 2 Question 3/src/page_rank.py
+++ A2/Assignment 2 Question 3/src/page_rank.py
@@ -11,31 +11,72 @@ Notes/thoughts:
  - Store and update the nodes (store id, PR, nodes it points to, etc.)
  - For each iteration is just re-loops through the existing stored nodes
 """
-def page_rank(maxiteration, lambda_, thr, nodes):
-    #test output to make sure variables are good
-    print("maxiteration= " + str(maxiteration) + ", lambda_= " + str(lambda_) + ", thr= " + str(thr) + ", nodes= " + str(nodes))
+class Node:
+    nodeId: int # id of the node
+    nodesPointTo: list[int] #nodes that this node points to
+    nodesPointFrom: list[int] # nodes that point to this node
+
+    def _init_(self, id):
+        self.nodeId = id
+
+    # adds nodeId to list of nodes that this node is linked to (points to)
+    def add_linked_node(self, nodeId):
+        if (nodeId not in self.nodesPointTo):
+            self.nodesPointTo.append(nodeId)
     
+    # adds nodeId to list of nodes that this node is linked to (points to)
+    def add_connected_node(self, nodeId):
+        if (nodeId not in self.nodesPointFrom):
+            self.nodesPointFrom.append(nodeId)
+
+
+def page_rank(maxiteration, lambda_, thr, nodes):
+    nodeList:list[Node] = []
+
+    # test output to make sure variables are good
+    print("maxiteration= " + str(maxiteration) + ", lambda_= " + str(lambda_) + ", thr= " + str(thr) + ", nodes= " + str(nodes))
+
     file = open("Web-Stanford.txt", "r")
     line = file.readline()
 
-    #get to actual nodes
+    # get to actual nodes
     while (line[0].isdigit() == False):
         line = file.readline()
 
-    #until end of file
+    # until end of file
     while (line != ""):
-        #extract node number and the node it points to
+        # extract node number and the node it points to
         extractedString = line.split()
-        nodeN = int(extractedString[0])
-        nodeConn = int(extractedString[1])
+        nodeId = int(extractedString[0])
+        linkedNode = int(extractedString[1])
 
-        #verifying I can get the numbers
-        print(str(nodeN) + " --> "+ str(nodeConn))
+        # verifying I can get the numbers
+        print(str(nodeId) + " --> " + str(linkedNode))
 
-        #next line
+        #add first node to list
+        if (nodeList.count == 0):
+            node = Node(nodeId)
+            node.add_linked_node(linkedNode)
+            nodeList.append(node)
+
+        else:
+            #if node was already added then just add to linked nodes
+            if (nodeId in nodeList):
+                node = Node(nodeList.index(nodeId))
+                node.add_linked_node(linkedNode)
+                #add to connected list of node this one links to
+            
+            #add new node
+            else:
+                node = Node(nodeId)
+                node.add_linked_node(linkedNode)
+                nodeList.append(node)
+                
+        # next line
         line = file.readline()
 
     file.close()
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
