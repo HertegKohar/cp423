773bfbcd5363e65ddccefaef5c373414dc10f7ae
Kelvin Kellner <kelvinkellner123@gmail.com>
2023-03-03 21:10:30 -0500

    Complete spell-checker, begin to reorganize code

--- A2/Assignment 2 Question 4/src/noisy_channel.py
+++ A2/Assignment 2 Question 4/src/noisy_channel.py
@@ -2,23 +2,66 @@
 Authors:
     Kelvin Kellner
     Herteg Kohar
-Resources:
-    https://norvig.com/spell-correct.html
-    https://medium.com/mlearning-ai/build-spell-checking-models-for-any-language-in-python-aa4489df0a5f
+Resource: https://norvig.com/spell-correct.html
 """
 import argparse
 from collections import Counter
 import json
 import nltk
-from nltk.tokenize import word_tokenize, RegexpTokenizer
-from nltk.corpus import stopwords, PlaintextCorpusReader
+from nltk.tokenize import RegexpTokenizer
+from nltk.corpus import stopwords
 import os
 import re
-import sys
+import string
 
-def words(text): return re.findall(r'\w+', text.lower())
+#TODO: confirm file name
+FILENAME = "wikipedia.token"
+DATA_FOLDER = "data_wikipedia"
 
-WORDS = Counter(words(open('wikipedia.token.stop', encoding="utf-8").read())) #TODO: confirm file name
+def tokenize(text):
+    "Return a list of all words from the text file."
+    return re.findall(r'\w+', text.lower())
+
+# term frequency vector for all words in the corpus
+# [(word: string, count: int), ...]
+WORD_FREQ_VECTOR = Counter(tokenize(open(FILENAME, encoding="utf-8").read()))
+
+def P(word): 
+    "Probability of 'word' occurring in the corpus."
+    N_TOKENS = sum(WORD_FREQ_VECTOR.values())
+    return WORD_FREQ_VECTOR[word] / N_TOKENS
+
+def correction(word): 
+    "Returns highest probability spelling correction canditate for 'word'."
+    return max(candidates(word), key=P)
+
+def candidates(word): 
+    "Return set of best spelling correction candidates for 'word'."
+    # candidates are generated using the following rules:
+    # 1. if 'word' is a 'known' word, return it as there is no need to correct it
+    # 2. otherise return the set of words that are only 1 edit distance away from 'word'
+    # 3. if there are none, then return the set of words that are only 2 edit distance away
+    # 4. if there are still none, then return the original 'word' as there is no correction to suggest
+    return (known_words([word]) or known_words(edit_distance_of_1(word)) or known_words(edit_distance_of_2(word)) or [word])
+
+def known_words(words): 
+    "The subset of 'words' that appear in the corpus, and thus, in 'WORD_FREQ_VECTOR'."
+    return set(w for w in words if w in WORD_FREQ_VECTOR)
+
+def edit_distance_of_1(word):
+    "All possible edits that are 1 edit distance away from 'word'."
+    alphabet = string.ascii_lowercase
+    pieces = [(word[:i], word[i:]) for i in range(len(word) + 1)]
+    insertions = [left + letter + right for left, right in pieces for letter in alphabet]
+    deletions = [left + right[1:] for left, right in pieces if right]
+    replacements = [left + letter + right[1:] for left, right in pieces if right for letter in alphabet]
+    transpositions = [left + right[1] + right[0] + right[2:] for left, right in pieces if len(right) > 1]
+    return set(insertions + deletions + replacements + transpositions)
+
+def edit_distance_of_2(word): 
+    "All possible edits that are two edits away from 'word'."
+    # compute all combinations of 1 more edit on all edits of 1 distance from 'word'
+    return (e2 for e1 in edit_distance_of_1(word) for e2 in edit_distance_of_1(e1))
 
 def load_articles(path):
     """Load articles from the given path to directory for wikipedia data
@@ -39,84 +82,52 @@ def load_articles(path):
                 if i % 1000 == 0:
                     print(f"Processed {i} articles")
             all_articles.extend(articles)
-        break
+        break # TODO: remove break to process all files
     return corpus, all_articles
 
-def remove_stop_words(tokens):
-    """Remove stopwords from the given corpus
+def process_and_save_corpus(tokens):
+    """Remove stopwords from the given corpus and output to a file
 
     Args:
         tokens (list[str]): List of tokens from the corpus
     """
-    # TODO: see if stopwords should be removed or not
-    # stop_words = set(stopwords.words("english"))
+    # stop_words = set(stopwords.words("english")) # TODO: see if stop word removal is needed
     stop_words = set()
     filtered_tokens = [token for token in tokens if not token.lower() in stop_words]
-    with open("wikipedia.token.stop", "a", encoding="utf-8") as f:
+    with open(FILENAME, "a", encoding="utf-8") as f:
         f.write(" ".join(filtered_tokens))
         f.write("\n")
     return
-
-def P(word, N=sum(WORDS.values())): 
-    "Probability of `word`."
-    return WORDS[word] / N
-
-def correction(word): 
-    "Most probable spelling correction for word."
-    return max(candidates(word), key=P)
-
-def candidates(word): 
-    "Generate possible spelling corrections for word."
-    return (known([word]) or known(edits1(word)) or known(edits2(word)) or [word])
-
-def known(words): 
-    "The subset of `words` that appear in the dictionary of WORDS."
-    return set(w for w in words if w in WORDS)
-
-def edits1(word):
-    "All edits that are one edit away from `word`."
-    letters    = 'abcdefghijklmnopqrstuvwxyz'
-    splits     = [(word[:i], word[i:])    for i in range(len(word) + 1)]
-    deletes    = [L + R[1:]               for L, R in splits if R]
-    transposes = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R)>1]
-    replaces   = [L + c + R[1:]           for L, R in splits if R for c in letters]
-    inserts    = [L + c + R               for L, R in splits for c in letters]
-    return set(deletes + transposes + replaces + inserts)
-
-def edits2(word): 
-    "All edits that are two edits away from `word`."
-    return (e2 for e1 in edits1(word) for e2 in edits1(e1))
-
-def noisy_channel(words):
-    '''
-    TODO: docstring
-    '''
-    for word in words:
-        print(f"Correcting {word} to {correction(word)}")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Noisy Channel Mode Spell-Checking Script")
     options = parser.add_mutually_exclusive_group(required=True)
-    options.add_argument('--correct', nargs="+", help="Words to display correct spelling for")
-    options.add_argument('--proba', nargs="+", help="Words to display probability of spell correction for")
+    options.add_argument('--correct', nargs="+", help="List of Words to spell-correct")
+    options.add_argument('--proba', nargs="+", help="List of Words to show proportion of occurrences in the corpus for")
     args = parser.parse_args()
 
+    # 1. Load the corpus
+    os.path.exists(FILENAME)
+    overwrite_file = input(f'''\nThe corpus has already been processed and stored in '{FILENAME}'.
+Would you like to overwrite the existing file and process it again? (approx. 2-5 mins - y/n): ''')
+    if overwrite_file.lower() == "y" or overwrite_file.lower() == "yes":
+        with open(FILENAME, "w", encoding="utf-8") as f:
+            pass
+        corpus, all_articles = load_articles(DATA_FOLDER)
+        tokens = RegexpTokenizer(r"\w+").tokenize(corpus)
+        process_and_save_corpus(tokens)
+
+    # 2. Output the correction or probability of the given words as needed
     mode = "correct" if args.correct is not None else "proba"
     words = args.correct if args.correct is not None else args.proba
-    print (mode, words)
-    # noisy_channel(words)
+    if mode == "correct":
+        for word in words:
+            print(f"{word} -> {correction(word)}")
+    if mode == "proba":
+        for word in words:
+            print(f"P({word}) = {P(word)}")
     
-    # TODO: integrate with rest of code
-    exit()
-    stopwords = True
-    if stopwords:
-        with open("wikipedia.token.stop", "w", encoding="utf-8") as f:
-            pass
-    corpus, all_articles = load_articles("../../Assignment 2 Question 1/src/data_wikipedia") # TODO: check file folder location
-    tokens = RegexpTokenizer(r"\w+").tokenize(corpus)
-    if stopwords:
-        print("Removing stopwords")
-        nltk.download("stopwords")
-        remove_stop_words(tokens)
-        print("Saved corpus without stopwords to wikipedia.token.stop")
+    # All done :)
+    print('\nProgram completed successfully.\n')
+    
