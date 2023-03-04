663bc551936b5e188c5491796775834b1aa0cfe6
Bryan Gadd <x-dragola-x@live.ca>
2023-03-03 19:36:52 -0500

    Testing the main file...
    Changes:
    - Swapped to for loop for best performance (thanks Herteg)

--- A2/Assignment 2 Question 3/src/page_rank.py
+++ A2/Assignment 2 Question 3/src/page_rank.py
@@ -3,6 +3,7 @@ import ast
 
 """
 Author: Bryan Gadd
+66MB
 """
 
 """
@@ -13,6 +14,8 @@ Notes/thoughts:
 """
 class Node:
     nodeId: int # id of the node
+    
+    #TODO- COnvert these to dictionaries (can do index finding much faster)
     nodesPointTo: list[int] #nodes that this node points to
     nodesPointFrom: list[int] # nodes that point to this node
 
@@ -51,66 +54,67 @@ def page_rank(maxiteration, lambda_, thr, nodes):
     # test output to make sure variables are good
     #print("maxiteration= " + str(maxiteration) + ", lambda_= " + str(lambda_) + ", thr= " + str(thr) + ", nodes= " + str(nodes))
 
-    file = open("test.txt", "r")
+    file = open("Web-Stanford.txt", "r")
     line = file.readline()
 
+    count = 0
+
     # get to actual nodes
-    while (line[0].isdigit() == False):
-        line = file.readline()
+    with open("Web-Stanford.txt") as f:
+        for line in f:
+            print (count)
+            count += 1
+            if (line[0].isdigit() == False):
+                continue
 
-    # until end of file
-    while (line != ""):
-        # extract node number and the node it points to
-        extractedString = line.split()
-        nodeId = int(extractedString[0])
-        linkedNode = int(extractedString[1])
+            # extract node number and the node it points to
+            extractedString = line.split()
+            nodeId = int(extractedString[0])
+            linkedNode = int(extractedString[1])
 
-        # verifying I can get the numbers
-        #print(str(nodeId) + " --> " + str(linkedNode))
+            # verifying I can get the numbers
+            print(str(nodeId) + " --> " + str(linkedNode))
 
-        #add first node to list
-        if (nodeList.count == 0):
-            node = Node(nodeId)
-            node.add_linked_node(linkedNode)
-            nodeList.append(node)
-
-        else:
-            #if node was already added then just add to linked nodes
-            if (node_exists(nodeList, nodeId)):
-                #print("Node '" + str(nodeId) +"' exists. Adding '" + str(linkedNode) + "' to it's list...")
-                node = node_in_list(nodeList, nodeId)
-                node.add_linked_node(linkedNode)
-                connectedNode = node_in_list(nodeList, linkedNode)
-                
-                #if node doesn't exist yet we need to add it
-                if (connectedNode == None):
-                    #print("Node '" + str(nodeId) +"' exists. Adding '" + str(linkedNode) + "' to it's list...")
-                    node = Node(linkedNode)
-                    node.add_connected_node(nodeId)
-                    nodeList.append(node)
-                else:
-                    connectedNode.add_connected_node(nodeId)
-            #add new node
-            else:
-                print("Node '" + str(nodeId) +"' doesn't exist. Adding it to the list with '" + str(linkedNode) + "' for the first linked node")
+            #add first node to list
+            if (nodeList.count == 0):
                 node = Node(nodeId)
                 node.add_linked_node(linkedNode)
                 nodeList.append(node)
-                connectedNode = node_in_list(nodeList, linkedNode)
 
-                #if node doesn't exist yet we need to add it
-                if (connectedNode == None):
+            else:
+                #if node was already added then just add to linked nodes
+                if (node_exists(nodeList, nodeId)):
                     #print("Node '" + str(nodeId) +"' exists. Adding '" + str(linkedNode) + "' to it's list...")
-                    node = Node(linkedNode)
-                    node.add_connected_node(nodeId)
-                    nodeList.append(node)
+                    node = node_in_list(nodeList, nodeId)
+                    node.add_linked_node(linkedNode)
+                    connectedNode = node_in_list(nodeList, linkedNode)
+                    
+                    #if node doesn't exist yet we need to add it
+                    if (connectedNode == None):
+                        #print("Node '" + str(nodeId) +"' exists. Adding '" + str(linkedNode) + "' to it's list...")
+                        node = Node(linkedNode)
+                        node.add_connected_node(nodeId)
+                        nodeList.append(node)
+                    else:
+                        connectedNode.add_connected_node(nodeId)
+                #add new node
                 else:
-                    connectedNode.add_connected_node(nodeId)
-        
-        print("Node '" + str(node.nodeId) + "' nodesPointTo is now: " + str(node.nodesPointTo))
-        
-        # next line
-        line = file.readline()
+                    #print("Node '" + str(nodeId) +"' doesn't exist. Adding it to the list with '" + str(linkedNode) + "' for the first linked node")
+                    node = Node(nodeId)
+                    node.add_linked_node(linkedNode)
+                    nodeList.append(node)
+                    connectedNode = node_in_list(nodeList, linkedNode)
+
+                    #if node doesn't exist yet we need to add it
+                    if (connectedNode == None):
+                        #print("Node '" + str(nodeId) +"' exists. Adding '" + str(linkedNode) + "' to it's list...")
+                        node = Node(linkedNode)
+                        node.add_connected_node(nodeId)
+                        nodeList.append(node)
+                    else:
+                        connectedNode.add_connected_node(nodeId)
+            
+            #print("Node '" + str(node.nodeId) + "' nodesPointTo is now: " + str(node.nodesPointTo))
 
     file.close()
 
