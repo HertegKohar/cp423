2ec9fef055e05462ed989ce85b23c12f4fb6410d
Herteg Kohar <koha5160@mylaurier.ca>
2023-03-22 20:31:13 -0400

    Finished part 1 of question 1 next is part 2

--- A3/Assignment 3 Question 1/training_sentiment.py
+++ A3/Assignment 3 Question 1/training_sentiment.py
@@ -1,13 +1,30 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from nltk.tokenize import RegexpTokenizer
+import nltk
 from nltk.corpus import stopwords
 import numpy as np
 import os
+import joblib
 from sklearn.model_selection import train_test_split, cross_validate
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeClassifier
+
+from sklearn.metrics import (
+    make_scorer,
+    accuracy_score,
+    precision_score,
+    recall_score,
+    f1_score,
+    confusion_matrix,
+    classification_report,
+    ConfusionMatrixDisplay,
+    RocCurveDisplay,
+)
 import argparse
-import nltk
 
 
 def concatenate_data(imdb, amazon, yelp):
@@ -49,6 +66,62 @@ def preprocess(text_data):
         text_data["sentence"], text_data["label"], test_size=0.2, random_state=0
     )
     return X_train, X_test, y_train, y_test
+
+
+def create_bag_of_words(X_train, X_test):
+    vectorizer = TfidfVectorizer()
+    X_train_tfidf = vectorizer.fit_transform(X_train)
+    X_test_tfidf = vectorizer.transform(X_test)
+    return X_train_tfidf, X_test_tfidf
+
+
+def cross_validate_and_train(model, X_train_tfidf, y_train):
+    scoring = {
+        "accuracy": make_scorer(accuracy_score),
+        "precision": make_scorer(precision_score),
+        "recall": make_scorer(recall_score),
+        "f1_score": make_scorer(f1_score),
+    }
+    K_folds = 5
+    cv_results = cross_validate(
+        model,
+        X_train_tfidf,
+        y_train,
+        cv=K_folds,
+        scoring=scoring,
+        return_estimator=True,
+    )
+    for i in range(K_folds):
+        print(f"Fold: {i+1}")
+        print(f"Accuracy: {cv_results['test_accuracy'][i]}")
+        print(f"Precision: {cv_results['test_precision'][i]}")
+        print(f"Recall: {cv_results['test_recall'][i]}")
+        print(f"F1 Score: {cv_results['test_f1_score'][i]}")
+        print("\n")
+    print("Mean Metrics")
+    print(f"Mean Accuracy: {np.mean(cv_results['test_accuracy'])}")
+    print(f"Mean Precision: {np.mean(cv_results['test_precision'])}")
+    print(f"Mean Recall: {np.mean(cv_results['test_recall'])}")
+    print(f"Mean F1 Score: {np.mean(cv_results['test_f1_score'])}")
+    print("\n")
+    mean_scores = {
+        metric: np.mean(cv_results[f"test_{metric}"]) for metric in scoring.keys()
+    }
+    best_metric = max(mean_scores, key=mean_scores.get)
+    best_estimator_index = np.argmax(cv_results["test_" + best_metric])
+    best_estimator = cv_results["estimator"][best_estimator_index]
+    # print(f"Best Estimator: {best_estimator_index}")
+    return best_estimator
+
+
+def test_model(model, X_test_tfidf, y_test):
+    print("Test Metrics")
+    y_pred = model.predict(X_test_tfidf)
+    print(classification_report(y_test, y_pred))
+    cm = confusion_matrix(y_test, y_pred)
+    ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=model.classes_).plot()
+    # RocCurveDisplay.from_predictions(y_test, y_pred)
+    plt.show()
 
 
 if __name__ == "__main__":
@@ -94,12 +167,28 @@ if __name__ == "__main__":
 
     data = concatenate_data(args.imdb, args.amazon, args.yelp)
     nltk.download("stopwords")
-
+    X_train, X_test, y_train, y_test = preprocess(data)
+    X_train_tfidf, X_test_tfidf = create_bag_of_words(X_train, X_test)
     if args.knn:
         print(f"Training KNN with K={args.k_value}")
+        clf = KNeighborsClassifier(n_neighbors=args.k_value)
+        output_path = f"knn_model_{args.k_value}.joblib"
     elif args.naive:
         print("Training Naive Bayes")
+        clf = GaussianNB()
+        X_train_tfidf = X_train_tfidf.toarray()
+        X_test_tfidf = X_test_tfidf.toarray()
+        output_path = "naive_model.joblib"
     elif args.svm:
         print("Training SVM")
+        clf = SVC(kernel="linear")
+        output_path = "svm_model.joblib"
     elif args.decisiontree:
         print("Training Decision Tree")
+        clf = DecisionTreeClassifier()
+        output_path = "decision_tree_model.joblib"
+
+    model = cross_validate_and_train(clf, X_train_tfidf, y_train)
+    test_model(model, X_test_tfidf, y_test)
+    joblib.dump(model, output_path)
+    print(f"Model saved to {output_path}")
